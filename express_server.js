99bd91e70ec5952e2edbffc816a50c5817dec13b
ApamAa <apama.jamshidi@gmail.com>
2019-12-10 22:20:31 +0000

    express_server.js

--- express_server.js
+++ express_server.js
@@ -7,6 +7,7 @@ const PORT = 8080; // default port 8080
 app.set("view engine", "ejs");
 const bodyParser = require("body-parser");
 app.use(bodyParser.urlencoded({extended: true}));
+
 const urlDatabase = {
   
   "b2xVn2": "http://www.lighthouselabs.ca",
@@ -55,14 +56,21 @@ app.get("/urls/:shortURL", (req, res) => {
   let templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL]};
   res.render("urls_show", templateVars)
 });
-app.post("/urls", (req, res) => {
-  console.log(req.body);  // Log the POST request body to the console
-  res.send(generateRandomString(6));         // Respond with 'Ok' (we will replace this)
-});
+
 app.get("/u/:shortURL", (req, res) => {
-  //let templateVars = { shortURL: req.params.shortUR
-  longURL= urlDatabase[req.params.shortURL];
-  res.redirect(longURL);
+  res.redirect(urlDatabase[req.params.shortURL])
+
+});
+
+app.post('/urls', (req, res) => {
+  let newShortUrl = generateRandomString(6);
+  res.redirect(`http://localhost:8080/urls/${newShortUrl}`);
+  urlDatabase[newShortUrl] = 'http://' + req.body.longURL;
+});
+
+app.post('/urls/:shortURL/delete',(req, res) => {
+   delete urlDatabase[req.params.shortURL];
+  res.redirect("/urls")
 });
 
 app.listen(PORT, () => {
